18ebdc1f32177f0a17bbeb5c328fa950cb80b887
Kaushalya13 <nimanthikaushalya13@gmail.com>
2023-10-29 12:24:59 +0530

    updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,16 +73,16 @@ var row_index = null;
 //---------Submit---------
 $("#item_btn>button[type='button']").eq(0).on("click", () => {
     let code = $("#code").val();
-    let name = $("#name").val();
+    let item_name = $("#item_name").val();
     let price = $("#price").val();
     let qty = $("#qty").val();
 
     console.log(code)
-    console.log(name)
+    console.log(item_name)
     console.log(price)
     console.log(qty)
 
-    let record = `<tr><td class="code">${code}</td><td class="name">${name}</td><td class="price">${price}</td><td class="qty">${qty}</td></tr>`;
+    let record = `<tr><td class="code">${code}</td><td class="item_name">${item_name}</td><td class="price">${price}</td><td class="qty">${qty}</td></tr>`;
     $("#item-tbl-body").append(record);
 
     $("#item_btn>button[type='reset']").click();
@@ -96,12 +96,12 @@ $("#item-tbl-body").on("click", "tr", function() {
     console.log(row_index);
 
     let code = $(this).find(".code").text();
-    let name = $(this).find(".name").text();
+    let item_name = $(this).find(".item_name").text();
     let price = $(this).find(".price").text();
     let qty = $(this).find(".qty").text();
 
     $("#code").val(code);
-    $("#name").val(name);
+    $("#item_name").val(item_name);
     $("#price").val(price);
     $("#qty").val(qty);
 })
@@ -109,16 +109,16 @@ $("#item-tbl-body").on("click", "tr", function() {
 //---------Update--------
 $("#item_btn>button[type='button']").eq(1).on("click", () => {
     let code = $("#code").val();
-    let name = $("#name").val();
+    let item_name = $("#item_name").val();
     let price = $("#price").val();
     let qty = $("#qty").val();
 
     console.log(code)
-    console.log(name)
+    console.log(item_name)
     console.log(price)
     console.log(qty)
 
-    let record = `<td class="code">${code}</td><td class="name">${name}</td><td class="price">${price}</td><td class="qty">${qty}</td>`;
+    let record = `<td class="code">${code}</td><td class="item_name">${item_name}</td><td class="price">${price}</td><td class="qty">${qty}</td>`;
     $("tbody>tr").eq(row_index).html(record);
 
     $("#item_btn>button[type='reset']").click();
@@ -137,16 +137,3 @@ $("#item_btn>button[type='button']").eq(2).on("click", () => {
 //----------------------------------Place order from-------------------------------------
 
 //-------------submit---------
-$("#place_order_btn>button[type='button']").eq(0).on("click", () => {
-    let item_id = $("#item_id").val();
-    let item_name = $("#item_name").val();
-    let quantity = $("#quantity").val();
-    let price = $("#price").val();
-    let description = $("#description").val();
-
-    let record = `<tr><td class="item_id">${item_id}</td><td class="item_name">${item_name}</td><td class="quantity">${quantity}</td><td class="price">${price}</td><td class="description">${description}</td></tr>`;
-    $("#item-tbl-body").append(record);
-
-    $("#place_order_btn>button[type='reset']").click();
-
-});
